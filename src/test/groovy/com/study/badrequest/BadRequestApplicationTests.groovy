68778e56e7174965f5c466600fd7021945479e22
cszzang89@gamail.com <cszzang89@gmail.com>
2022-12-28 00:23:56 +0900

    Initial commit

---
+++ src/test/groovy/com/study/badrequest/BadRequestApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.study.badrequest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BadRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
