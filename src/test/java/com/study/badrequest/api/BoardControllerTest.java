8fe873530b0f1fda9d45e3db743f83705de8e4e5
cszzang89@gamail.com <cszzang89@gmail.com>
2023-02-06 22:45:39 +0900

    Board Api 문서 추가

--- src/test/java/com/study/badrequest/api/BoardControllerTest.java
+++ src/test/java/com/study/badrequest/api/BoardControllerTest.java
@@ -28,7 +28,8 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.headerWit
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
@@ -106,6 +107,69 @@ class BoardControllerTest {
                                 fieldWithPath("result.links.[0].href").type(JsonFieldType.STRING).description("링크")
                         )
                 ));
+    }
+
+    @Test
+    @DisplayName("게시판 리스트 조회")
+    void getBoardTest() throws Exception {
+        //given
+
+        //when
+        mockMvc.perform(get("/api/v1/board"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("status").value(CustomStatus.SUCCESS.name()))
+                .andExpect(jsonPath("code").value(CustomStatus.SUCCESS.getCode()))
+                .andExpect(jsonPath("message").value(CustomStatus.SUCCESS.getMessage()))
+                .andExpect(jsonPath("result.size").exists())
+                .andExpect(jsonPath("result.hasNext").exists())
+                .andExpect(jsonPath("result.lastIndex").exists())
+                .andExpect(jsonPath("result.results").exists())
+                .andExpect(jsonPath("result.results.[0].boardId").exists())
+                .andExpect(jsonPath("result.results.[0].memberId").exists())
+                .andExpect(jsonPath("result.results.[0].profileImage").isEmpty())
+                .andExpect(jsonPath("result.results.[0].nickname").exists())
+                .andExpect(jsonPath("result.results.[0].title").exists())
+                .andExpect(jsonPath("result.results.[0].likeCount").exists())
+                .andExpect(jsonPath("result.results.[0].category").exists())
+                .andExpect(jsonPath("result.results.[0].topic").exists())
+                .andExpect(jsonPath("result.results.[0].commentCount").exists())
+                .andExpect(jsonPath("result.results.[0].createdAt").exists())
+                .andExpect(jsonPath("result.results.[0].links").exists())
+                .andExpect(jsonPath("result.results.[0].links.[0].rel").exists())
+                .andExpect(jsonPath("result.results.[0].links.[0].href").exists())
+                .andExpect(jsonPath("result.links").exists())
+                .andExpect(jsonPath("result.links.[0].href").exists())
+                .andExpect(jsonPath("result.links.[0].rel").exists())
+                .andDo(print())
+
+                //then
+                .andDo(document("board_getList_no_condition",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("status").type(JsonFieldType.STRING).description("커스텀 응답상태"),
+                                fieldWithPath("code").type(JsonFieldType.NUMBER).description("커스텀 응답 코드"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("커스텀 응답 메시지"),
+                                fieldWithPath("result.size").type(JsonFieldType.NUMBER).description("리스트 사이즈"),
+                                fieldWithPath("result.hasNext").type(JsonFieldType.BOOLEAN).description("다음 데이터 유무"),
+                                fieldWithPath("result.lastIndex").type(JsonFieldType.NUMBER).description("요청 데이터 마지막 인덱스"),
+                                fieldWithPath("result.results.[0].boardId").type(JsonFieldType.NUMBER).description("게시판 아이디"),
+                                fieldWithPath("result.results.[0].memberId").type(JsonFieldType.NUMBER).description("회원 아이디"),
+                                fieldWithPath("result.results.[0].profileImage").type(JsonFieldType.STRING).description("프로필 이미지").optional(),
+                                fieldWithPath("result.results.[0].nickname").type(JsonFieldType.STRING).description("닉네임"),
+                                fieldWithPath("result.results.[0].title").type(JsonFieldType.STRING).description("제목"),
+                                fieldWithPath("result.results.[0].likeCount").type(JsonFieldType.NUMBER).description("좋아요 개수"),
+                                fieldWithPath("result.results.[0].category").type(JsonFieldType.STRING).description("카테고리"),
+                                fieldWithPath("result.results.[0].topic").type(JsonFieldType.STRING).description("주제"),
+                                fieldWithPath("result.results.[0].commentCount").type(JsonFieldType.NUMBER).description("댓글 개수"),
+                                fieldWithPath("result.results.[0].createdAt").type(JsonFieldType.STRING).description("게시판 생성일"),
+                                fieldWithPath("result.results.[0].links.[0].rel").type(JsonFieldType.STRING).description("링크 설명"),
+                                fieldWithPath("result.results.[0].links.[0].href").type(JsonFieldType.STRING).description("링크"),
+                                fieldWithPath("result.links.[0].rel").type(JsonFieldType.STRING).description("링크 설명"),
+                                fieldWithPath("result.links.[0].href").type(JsonFieldType.STRING).description("링크")
+                        )
+                ));
+
     }
 
 }
