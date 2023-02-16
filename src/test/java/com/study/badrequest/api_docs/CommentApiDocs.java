11fcb5f20a79610518c7bdaa3d579a8a1c79f2a8
cszzang89@gamail.com <cszzang89@gmail.com>
2023-02-16 21:59:33 +0900

    Comment API Docs 추가

--- src/test/java/com/study/badrequest/api_docs/CommentApiDocs.java
+++ src/test/java/com/study/badrequest/api_docs/CommentApiDocs.java
@@ -223,4 +223,59 @@ class CommentApiDocs {
                                 fieldWithPath("result.links.[0].href").type(JsonFieldType.STRING).description("링크")
                         )));
     }
+
+    @Test
+    @DisplayName("댓글 조회 조건 없이")
+    void getComments() throws Exception {
+        //given
+        String email = "tester@test.com";
+        Member member = memberRepository.findByEmail(email).get();
+        Board board = boardRepository.findByTitle("제목").get();
+
+        CommentRequest.Create create1 = new CommentRequest.Create("댓글 1번 입니다.");
+        CommentRequest.Create create2 = new CommentRequest.Create("댓글 2번 입니다.");
+        CommentRequest.Create create3 = new CommentRequest.Create("댓글 3번 입니다.");
+        CommentRequest.Create create4 = new CommentRequest.Create("댓글 4번 입니다.");
+        CommentRequest.Create create5 = new CommentRequest.Create("댓글 5번 입니다.");
+        CommentRequest.Create create6 = new CommentRequest.Create("댓글 6번 입니다.");
+
+        commentCommendService.addComment(board.getId(), member.getUsername(), create1);
+        commentCommendService.addComment(board.getId(), member.getUsername(), create2);
+        commentCommendService.addComment(board.getId(), member.getUsername(), create3);
+        commentCommendService.addComment(board.getId(), member.getUsername(), create4);
+        commentCommendService.addComment(board.getId(), member.getUsername(), create5);
+        commentCommendService.addComment(board.getId(), member.getUsername(), create6);
+        //when
+        mockMvc.perform(get("/api/v1/board/{boardId}/comments", board.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+
+                //then
+                .andDo(print())
+                .andDo(document("comment-get",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("status").type(JsonFieldType.STRING).description("커스텀 응답상태"),
+                                fieldWithPath("code").type(JsonFieldType.NUMBER).description("커스텀 응답 코드"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("커스텀 응답 메시지"),
+                                fieldWithPath("result.commentSize").type(JsonFieldType.NUMBER).description("댓글 개수"),
+                                fieldWithPath("result.hasNext").type(JsonFieldType.BOOLEAN).description("다음 댓글 존재 유무"),
+                                fieldWithPath("result.lastIndex").type(JsonFieldType.NUMBER).description("마지막 댓글 인덱스"),
+                                fieldWithPath("result.links.[0].rel").type(JsonFieldType.STRING).description("링크 설명"),
+                                fieldWithPath("result.links.[0].href").type(JsonFieldType.STRING).description("링크"),
+                                fieldWithPath("result.results.[0].commentId").type(JsonFieldType.NUMBER).description("댓글 아이디"),
+                                fieldWithPath("result.results.[0].boardId").type(JsonFieldType.NUMBER).description("게시판 아이디"),
+                                fieldWithPath("result.results.[0].memberId").type(JsonFieldType.NUMBER).description("회원 아이디"),
+                                fieldWithPath("result.results.[0].profileImage").type(JsonFieldType.STRING).description("프로필 이미지 경로"),
+                                fieldWithPath("result.results.[0].nickname").type(JsonFieldType.STRING).description("닉네임"),
+                                fieldWithPath("result.results.[0].text").type(JsonFieldType.STRING).description("댓글 내용"),
+                                fieldWithPath("result.results.[0].likeCount").type(JsonFieldType.NUMBER).description("좋아요 개수"),
+                                fieldWithPath("result.results.[0].subCommentCount").type(JsonFieldType.NUMBER).description("대댓글 개수"),
+                                fieldWithPath("result.results.[0].createdAt").type(JsonFieldType.STRING).description("댓글 등록일"),
+                                fieldWithPath("result.results.[0].updatedAt").type(JsonFieldType.STRING).description("댓글 수정일"),
+                                fieldWithPath("result.results.[0].links.[0].rel").type(JsonFieldType.STRING).description("링크 설명"),
+                                fieldWithPath("result.results.[0].links.[0].href").type(JsonFieldType.STRING).description("링크")
+                        )));
+    }
 }
